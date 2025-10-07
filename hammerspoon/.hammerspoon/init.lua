807c79a225e87375f84205c4d81e75527fc553fe
Илья Лисин <blacklacost@gmail.com>
2025-10-07 16:35:18 +0300

    add hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,8 +1,11 @@
 for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
-    { shortcut = "2", name = "Brave" },
+    { shortcut = "2", name = "Brave Browser" },
     { shortcut = "3", name = "Todoist" },
     { shortcut = "4", name = "Obsidian" },
+    { shortcut = "5", name = "IntelliJ IDEA" },
+    { shortcut = "6", name = "Anki" },
+    { shortcut = "7", name = "Streisand" },
     { shortcut = "8", name = "Telegram" },
     { shortcut = "9", name = "Calendar" },
     { shortcut = "0", name = "Mail" },
