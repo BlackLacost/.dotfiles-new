e94bd3001b94d805739881ea99f3766ee690a636
Илья Лисин <blacklacost@gmail.com>
2025-10-10 07:41:38 +0300

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local config = {}
+
+config.font_size = 20
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
+return config 
